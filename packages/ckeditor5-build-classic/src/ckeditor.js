1093e611a2c3d6de479324ed3ec634c458e1743d
Leon Marzahn <l.marzahn@nexnox.com>
2021-09-03 14:05:21 +0200

    Add underline plugin

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -11,6 +11,7 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -40,6 +41,7 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+    Underline,
 	BlockQuote,
 	CKFinder,
 	CloudServices,
@@ -69,6 +71,7 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+		    'underline',
 			'link',
 			'bulletedList',
 			'numberedList',
